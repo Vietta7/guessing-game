7a19de302dbb2e7d9d7ae14d2b2a227184f72aa1
Anna <annaspacer7@gmail.com>
2024-08-30 13:27:33 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minRange = 0;
+        this.maxRange = 0;
+        this.currentGuess = 0;
+    }
 
     setRange(min, max) {
-
+        this.minRange = min;
+        this.maxRange = max;
     }
-
+    // Вернули текущее предположение
     guess() {
-
+        this.currentGuess = Math.round((this.minRange + this.maxRange)/2);
+        return this.currentGuess;
     }
-
+    // Обновили границы диапазона
     lower() {
-
+        this.maxRange = this.currentGuess;
     }
 
     greater() {
-
+        this.minRange = this.currentGuess;
     }
 }
 
